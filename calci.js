291234e6382df116fb866caa40d27c21a2b8880f
Eswar <166409699+Eswar565@users.noreply.github.com>
2026-07-21 11:27:19 +0530

    Update calci.js

--- calci.js
+++ calci.js
@@ -1,63 +1,119 @@
 const MAX = 2147483647;
 const MIN = -2147483648;
 
-let result = 0;
+let previousResult = 0;
 
-function checkRange(value) {
-    return value >= MIN && value <= MAX;
+function isOutOfRange(a, b) {
+    return a > MAX || b > MAX || a < MIN || b < MIN;
 }
 
 function add(a, b) {
     if (b === undefined) {
-        result = result + a;
-    } else {
-        result = a + b;
+        b = a;
+        a = previousResult;
     }
+
+    if (isOutOfRange(a, b)) {
+        return "out of range";
+    }
+
+    let result = a + b;
+
+    if (result > MAX || result < MIN) {
+        return "out of range";
+    }
+
+    previousResult = result;
     return result;
 }
 
 function subtract(a, b) {
     if (b === undefined) {
-        result = result - a;
-    } else {
-        result = a - b;
+        b = a;
+        a = previousResult;
     }
+
+    if (isOutOfRange(a, b)) {
+        return "out of range";
+    }
+
+    let result = a - b;
+
+    if (result > MAX || result < MIN) {
+        return "out of range";
+    }
+
+    previousResult = result;
     return result;
 }
 
 function multiply(a, b) {
     if (b === undefined) {
-        result = result * a;
-    } else {
-        result = a * b;
+        b = a;
+        a = previousResult;
     }
+
+    if (isOutOfRange(a, b)) {
+        return "out of range";
+    }
+
+    let result = a * b;
+
+    if (result > MAX || result < MIN) {
+        return "out of range";
+    }
+
+    previousResult = result;
     return result;
 }
 
 function divide(a, b) {
     if (b === undefined) {
-        if (a === 0) return "Cannot divide by zero";
-        result = result / a;
-    } else {
-        if (b === 0) return "Cannot divide by zero";
-        result = a / b;
+        b = a;
+        a = previousResult;
     }
+
+    if (b === 0) {
+        return "Cannot divide by zero";
+    }
+
+    if (isOutOfRange(a, b)) {
+        return "out of range";
+    }
+
+    let result = a / b;
+
+    if (result > MAX || result < MIN) {
+        return "out of range";
+    }
+
+    previousResult = result;
     return result;
 }
 
 function power_of(a, b) {
     if (b === undefined) {
-        result = result ** a;
-    } else {
-        result = a ** b;
+        b = a;
+        a = previousResult;
     }
+
+    if (isOutOfRange(a, b)) {
+        return "out of range";
+    }
+
+    let result = Math.pow(a, b);
+
+    if (result > MAX || result < MIN) {
+        return "out of range";
+    }
+
+    previousResult = result;
     return result;
 }
 
-// Testing
-console.log(add(2, 3));      
-console.log(add(3));
-console.log(multiply(2));    
-console.log(subtract(6));  
-console.log(divide(2));      
-console.log(power_of(2));   
+console.log("Add: " + add(2, 3));
+console.log("Add: " + add(5));
+console.log("Mul: " + multiply(2));
+console.log("Sub: " + subtract(5));
+console.log("Div: " + divide(3));
+console.log("Power: " + power_of(2));
